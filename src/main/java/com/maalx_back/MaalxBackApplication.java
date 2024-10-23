1736fbe52de325b36ad04ad9e09365e1a968a31f
yoon hyeon soo <chris24da@naver.com>
2024-10-23 18:47:05 +0900

    first commit

---
+++ src/main/java/com/maalx_back/MaalxBackApplication.java
@@ -0,0 +1,13 @@
+package com.maalx_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaalxBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaalxBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maalx_back/MaalxBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maalx_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaalxBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
