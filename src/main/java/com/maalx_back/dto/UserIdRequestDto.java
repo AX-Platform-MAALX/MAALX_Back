8394965845199d52b218718ee8857a2781b758bd
yeongseo <yeongseo5952@naver.com>
2024-11-21 01:29:05 +0900

    FEAT: 컨설팅 횟수 반환해주는 기능
    ## 작업 내용
    userId별 컨설팅 횟수 반환해 주는 기능 추가

---
+++ src/main/java/com/maalx_back/dto/UserIdRequestDto.java
@@ -0,0 +1,15 @@
+package com.maalx_back.dto;
+
+public class UserIdRequestDto {
+
+    private Long userId;
+
+    // Getter & Setter
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
